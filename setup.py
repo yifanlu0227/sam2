9adad043ea42b484bdad23847b12d47f6dd1d348
Yifan Lu <458887872@qq.com>
2024-12-06 08:09:44 +0000

    [yfl] remove torch constraint

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 # Required dependencies
 REQUIRED_PACKAGES = [
-    "torch>=2.3.1",
-    "torchvision>=0.18.1",
     "numpy>=1.24.4",
     "tqdm>=4.66.1",
     "hydra-core>=1.3.2",
